6f767a4b510481fafde480524f9214b4dce982d0
Postmaster <ohiocity@hawks.dreamhost.com>
2011-05-05 16:57:50 -0700

    removed updatemembers.sh because we are not tracking it... for now... cuz it has db credentials in it.

--- updatemembers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# do stuff here
-echo "Memberships that expired today:"
-mysql -u ocbc_mysql_root -p'f^&f,9asd*' -h mysql.ocbc.dreamhosters.com ohiocity_joomla < /home/ohiocity/www/ohiocitycycles.org/update_member_status.sql
