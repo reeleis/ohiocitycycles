c6f7d6d45c1896733960af0fe9a4d08123fde778
Postmaster <ohiocity@hawks.dreamhost.com>
2011-05-05 16:48:58 -0700

    membership expiration script

---
+++ update_member_status.sql
@@ -0,0 +1,7 @@
+SELECT nameFirst, nameLast, emailAddress, `membershipExpire`
+FROM `jos_cbodb_members`
+WHERE `membershipExpire`<=CURRENT_TIMESTAMP AND `isMember` = 1;
+
+UPDATE `jos_cbodb_members`
+SET `isMember` = 0
+WHERE `membershipExpire`<=CURRENT_TIMESTAMP AND `isMember` = 1;
